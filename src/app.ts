79d90a148348be0ca424e6b08193a70dba8639ec
davidtaing <adavidtaing@gmail.com>
2021-08-20 23:19:26 +1000

    changed input data into an array of Emails

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,43 @@
 import * as dotenv from 'dotenv';
+dotenv.config();
+
 import * as sgMail from '@sendgrid/mail';
 
-dotenv.config();
+import { Email } from './Email';
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const msg = {
-  to: 'teeang94@gmail.com',
-  from: 'adavidtaing@gmail.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
+const dataArray: Email[] = [
+  {
+    to: 'teeang94@gmail.com',
+    from: 'adavidtaing@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  },
+  {
+    to: 'teeang94@gmail.com',
+    from: 'adavidtaing@gmail.com',
+    subject: 'Sending second email SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  },
+  {
+    to: 'teeang94@gmail.com',
+    from: 'adavidtaing@gmail.com',
+    subject: 'Sending with Node.js is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  },
+];
 
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent');
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function sendEmail(msg) {
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
