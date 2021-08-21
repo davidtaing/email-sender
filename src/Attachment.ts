11b8919f8354b93ab98e303d492a0284a27d3b2b
davidtaing <adavidtaing@gmail.com>
2021-08-21 20:15:53 +1000

    added LoadAttachment function

--- src/Attachment.ts
+++ src/Attachment.ts
@@ -1,3 +1,7 @@
+import { readFile } from "fs/promises";
+import * as path from "path";
+import { mime } from "mime-types";
+
 export interface Attachment {
   content: string;
   filename: string;
@@ -5,3 +9,21 @@ export interface Attachment {
   disposition?: string;
   content_id?: string;
 }
+
+export function LoadAttachment(filepath: string): Attachment {
+  readFile(filepath)
+    .then((data) => {
+      const attachment: Attachment = {
+        content: data.toString('base64'),
+        filename: path.basename(filepath),
+        type: mime.lookup(filepath),
+      }
+      return attachment;
+    })
+    .catch((err) => {
+      console.log("Failed to load attachment");
+      console.log(err);
+    });
+
+    return null;
+};
