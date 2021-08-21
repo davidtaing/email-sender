ee80099194b615fb95f6007949f1edcfa661af5f
davidtaing <adavidtaing@gmail.com>
2021-08-21 21:48:54 +1000

    implemented createEmail function

--- src/Email.ts
+++ src/Email.ts
@@ -1,7 +1,11 @@
-import { Attachment } from './Attachment';
+import { fileURLToPath } from 'url';
+import { Attachment, loadAttachment } from './Attachment';
+
+const FROM_EMAIL: string = process.env.SENDGRID_FROM_EMAIL;
+const TEMPLATE_ID: string = process.env.SENDGRID_TEMPLATE_ID;
 
 export interface Email {
-  to: string;
+  to: string[];
   from: string;
   subject?: string;
   text?: string;
@@ -11,23 +15,29 @@ export interface Email {
   dynamicTemplateData?: {};
 }
 
-// This is intended to help me plan on how to create a data structure for our Email POJO
-export function createEmail({firstName, to, property, filepaths}): Email {
+// Init Email object to send to the SendGrid Email API
+export function createEmail({
+  firstName, 
+  to, 
+  property, 
+  filepaths
+}, 
+  from = FROM_EMAIL, 
+  templateId = TEMPLATE_ID,
+): Email {
   return {
-    to: '',
-    from: '',
-    attachments: [
-      {
-        content: '',
-        filename: '',
-        type: '',
-        disposition: 'attachment',
-      }
-    ],
-    templateId: '',
+    to: splitDelimitedString(to),
+    from: from,
+    attachments: splitDelimitedString(filepaths).map(filepath => loadAttachment(filepath)),
+    templateId: templateId,
     dynamicTemplateData: {
-      firstName: '',
-      property: '',
+      firstName: firstName,
+      property: property,
     }
   };
+}
+
+// Allows us to convert the email and attachment cells to be converted into arrays.
+function splitDelimitedString(string: string) : string[] {
+  return string.split(';');
 }
