7ce59f6bb62b29c71035af1ddfd98cc1ce33717d
davidtaing <adavidtaing@gmail.com>
2021-08-21 19:34:03 +1000

    adjusted column names in the spreadsheet

--- src/Email.ts
+++ src/Email.ts
@@ -3,8 +3,31 @@ import { Attachment } from './Attachment';
 export interface Email {
   to: string;
   from: string;
-  subject: string;
+  subjec?: string;
   text?: string;
   html?: string;
   attachments?: Attachment[];
+  templateId: string;
+  dymanicTemplateData: {};
+}
+
+// This is intended to help me plan on how to create a data structure for our Email POJO
+export function CreateEmail(): Email {
+  return {
+    to: '',
+    from: '',
+    attachments: [
+      {
+        content: '',
+        filename: '',
+        type: '',
+        disposition: 'attachment',
+      }
+    ],
+    templateId: '',
+    dymanicTemplateData: {
+      firstName: '',
+      property: '',
+    }
+  };
 }
