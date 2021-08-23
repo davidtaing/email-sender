d330e497394160a1cb768231b02e2dcf12891e39
davidtaing <adavidtaing@gmail.com>
2021-08-23 21:25:18 +1000

    Refactored out parsing email data from XLSX
    Also adjusted strings to use double quotations and added missing semi-colons.

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,30 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import * as sgMail from '@sendgrid/mail';
-import * as XLSX from 'XLSX';
-import { createEmail } from './Email';
+import * as sgMail from "@sendgrid/mail";
+import * as XLSX from "XLSX";
+import { Email, createEmail } from "./Email";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const XLSX_PATH: string = process.env.DATA_SOURCE_XLSX;
 
-const parsedEmailData = loadXLSXToJSON().map((data: any) => {
-  let { firstName, to, property, filepaths } = data;
-  return createEmail(firstName, to, property, filepaths);
-});
+const RAW_XLSX_DATA = loadXLSXToJSON(XLSX_PATH);
+
+const PARSED_EMAIL_DATA = 
+  parseEmailsFromXLSX(RAW_XLSX_DATA)
+    .then(
+      data => Promise.resolve(data)
+    )
+    .catch(e => {
+      console.log("Failed to parse spreadsheet data.");
+      console.log(e);
+    });
 
 function sendEmail(msg) {
   sgMail
     .send(msg)
     .then(() => {
-      console.log('Email sent');
+      console.log("Email sent");
     })
     .catch((error) => {
       console.error(error);
@@ -28,11 +35,11 @@ function sendEmail(msg) {
 function loadXLSXToJSON(path: string = XLSX_PATH) {
   try {
     let wb = XLSX.readFile(path);
-    let firstSheetName = wb.SheetNames[0]
+    let firstSheetName = wb.SheetNames[0];
     let ws = wb.Sheets[firstSheetName];
 
     if (ws === undefined) {
-      throw 'Failed to load spreadsheet.';
+      throw "Failed to load spreadsheet.";
     }
 
     return XLSX.utils.sheet_to_json(ws, {raw: false});
@@ -42,5 +49,19 @@ function loadXLSXToJSON(path: string = XLSX_PATH) {
   }
 }
 
+// Parses spreadsheet data into SendGrid Email API POJO (or Email Objects)
+async function parseEmailsFromXLSX(data = RAW_XLSX_DATA) {
+  let emails : Array<Email> = [];
+
+  let item : any;
+  for (item of data) {
+    let { firstName, to, property, filepaths } = item;
+
+    emails.push(await createEmail(firstName, to, property, filepaths));
+  }
+  
+  return emails;
+};
+
 // Output parsed data.
-console.log(parsedEmailData);
+console.log(PARSED_EMAIL_DATA);
